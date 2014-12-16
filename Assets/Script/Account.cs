e590adbd3f5be0b0585583507cf7bca126d40267
minh tan <m.tan2007@gmail.com>
2014-12-16 20:45:28 +0700

    working on user sign up/sign in

---
+++ Assets/Script/Account.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Parse;
+
+public class Account : MonoBehaviour {
+	public GameObject usernameInput;
+	public GameObject passwordInput;
+
+	private void setUsernameInput(string input){
+		Text text = usernameInput.GetComponent<Text> ();
+		text.text = input;
+	}
+
+	private string getUsernameInput(){
+		return usernameInput.GetComponent<Text> ().text;
+	}
+
+	private string getPasswordInput(){
+		return passwordInput.GetComponent<Text> ().text;
+	}
+
+	public void signUp(){
+		string username = getUsernameInput ();
+		string password = getPasswordInput ();
+		IDictionary<string,object> dict = new Dictionary<string, object>()
+		{
+			{"username", username},
+			{"password", password}
+		};
+
+		ParseCloud.CallFunctionAsync<IDictionary<string, object>> ("signUp", dict)
+			.ContinueWith (t =>
+				{
+				if (t.IsFaulted){
+					using (IEnumerator<System.Exception> enumerator = t.Exception.InnerExceptions.GetEnumerator()) {
+						if (enumerator.MoveNext()) {
+							ParseException error = (ParseException) enumerator.Current;
+							Debug.Log("Error: " + error.Code + ", " + error.Message);
+						}
+					}
+				}else{
+					IDictionary<string, object> result = t.Result;
+					object errorCode;
+					if (result.TryGetValue("errorCode", out errorCode)) {
+						Debug.Log ("Error: " + result["errorCode"] + ", " + result["message"]);
+					} else {
+						Debug.Log ("Success: " + result["successCode"]);
+					}
+				}
+			});
+	}
+
+}
