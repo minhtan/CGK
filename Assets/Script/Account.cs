780f488c5ab91a0696af9dd9dfb7cc28f6dd9d7b
minh tan <m.tan2007@gmail.com>
2014-12-18 17:05:04 +0700

    Finish sign in/out function

--- Assets/Script/Account.cs
+++ Assets/Script/Account.cs
@@ -9,11 +9,6 @@ public class Account : MonoBehaviour {
 	public GameObject usernameInput;
 	public GameObject passwordInput;
 
-	private void setUsernameInput(string input){
-		Text text = usernameInput.GetComponent<Text> ();
-		text.text = input;
-	}
-
 	private string getUsernameInput(){
 		return usernameInput.GetComponent<Text> ().text;
 	}
@@ -31,26 +26,48 @@ public class Account : MonoBehaviour {
 			{"password", password}
 		};
 
-		ParseCloud.CallFunctionAsync<IDictionary<string, object>> ("signUp", dict)
-			.ContinueWith (t =>
-				{
-				if (t.IsFaulted){
-					using (IEnumerator<System.Exception> enumerator = t.Exception.InnerExceptions.GetEnumerator()) {
-						if (enumerator.MoveNext()) {
-							ParseException error = (ParseException) enumerator.Current;
-							Debug.Log("Error: " + error.Code + ", " + error.Message);
-						}
-					}
-				}else{
-					IDictionary<string, object> result = t.Result;
-					object errorCode;
-					if (result.TryGetValue("errorCode", out errorCode)) {
-						Debug.Log ("Error: " + result["errorCode"] + ", " + result["message"]);
-					} else {
-						Debug.Log ("Success: " + result["successCode"]);
+		ParseCloud.CallFunctionAsync<IDictionary<string, object>> ("signUp", dict).ContinueWith (t => {
+			if (t.IsFaulted){
+				using (IEnumerator<System.Exception> enumerator = t.Exception.InnerExceptions.GetEnumerator()) {
+					if (enumerator.MoveNext()) {
+						ParseException error = (ParseException) enumerator.Current;
+						Debug.Log("Error: " + error.Code + ", " + error.Message);
 					}
 				}
-			});
+			}else{
+				IDictionary<string, object> result = t.Result;
+				object errorCode;
+				if (result.TryGetValue("errorCode", out errorCode)) {
+					Debug.Log ("Error: " + result["errorCode"] + ", " + result["message"]);
+				} else {
+					Debug.Log ("Success: " + result["successCode"]);
+				}
+			}
+		});
 	}
 
+	public void signIn(){
+		string username = getUsernameInput ();
+		string password = getPasswordInput ();
+		ParseUser.LogInAsync (username, password).ContinueWith (t => {
+			if (t.IsFaulted || t.IsCanceled) {
+				Debug.Log ("Sign in failed");
+			} else {
+				Debug.Log ("Sign in successfully");
+			}
+		});
+	}
+
+	public bool checkCurrentUser(){
+		if (ParseUser.CurrentUser != null){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public void signOut(){
+		ParseUser.LogOut();
+	}
+	
 }
