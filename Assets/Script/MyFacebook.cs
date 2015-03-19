f9298058b9dbcf820abe0c25bfce540bf23cda2a
minh tan <m.tan2007@gmail.com>
2015-03-19 16:27:22 +0700

    add FB script

---
+++ Assets/Script/MyFacebook.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook;
+
+public class MyFacebook : MonoBehaviour {
+
+    private void SetInit()
+    {
+        enabled = true;
+        // "enabled" is a magic global; this lets us wait for FB before we start rendering
+    }
+
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else
+        {
+            // start the game back up - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+
+	// Use this for initialization
+	void Awake () {
+        // Initialize FB SDK              
+        enabled = false;
+        FB.Init(SetInit, OnHideUnity);
+	}
+	
+	// Update is called once per frame
+	public void loginFB() {
+        if (!FB.IsLoggedIn)
+        {
+            FB.Login("", LoginCallback);
+        }
+	}
+
+    void LoginCallback(FBResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            OnLoggedIn();
+        }
+    }
+
+    void OnLoggedIn()
+    {
+        Debug.Log("Logged in");
+    }  
+}
